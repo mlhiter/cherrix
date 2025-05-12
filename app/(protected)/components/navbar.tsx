8022e91d5e2ad19de167cdea86cbc2a9423e9d44
mlhiter <3076438032@qq.com>
2025-05-12 23:20:07 +0800

    fix(home): adjust title click button

--- app/(protected)/components/navbar.tsx
+++ app/(protected)/components/navbar.tsx
@@ -1,12 +1,18 @@
 'use client'
 
 import { UserButton } from '@/components/auth/user-button'
+import { useRouter } from 'next/navigation'
 
 export const Navbar = () => {
+  const router = useRouter()
   return (
     <div className="w-full items-center justify-between bg-secondary p-4 shadow-sm">
       <div className="flex items-center justify-between gap-x-2">
-        <div className="flex gap-x-2 text-2xl font-bold">🍒 Cherrix</div>
+        <div
+          className="flex gap-x-2 text-2xl font-bold hover:cursor-pointer hover:text-primary"
+          onClick={() => router.push('/client/home')}>
+          🍒 Cherrix
+        </div>
         <UserButton />
       </div>
     </div>
